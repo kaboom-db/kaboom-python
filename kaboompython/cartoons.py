fa1e84cda7617e3c4ea4ab4ed983f6646052789b
Tanveer Najib <tanveernajib@gmail.com>
2022-03-14 13:33:45 +0000

    added episode client

--- kaboompython/cartoons.py
+++ kaboompython/cartoons.py
@@ -4,7 +4,7 @@ class CartoonsClient(Base):
     def get_cartoons(self, **kwargs):
         return self.request(endpoint='v1/cartoons/series/', params=kwargs)
 
-    def add_cartoons(self, **kwargs):
+    def add(self, **kwargs):
         return self.request(endpoint='v1/cartoons/series/', method='POST', data=kwargs)
     
     def detail(self, cartoon_id: int):
@@ -12,3 +12,16 @@ class CartoonsClient(Base):
     
     def update(self, cartoon_id: int, **kwargs):
         return self.request(endpoint=f'v1/cartoons/series/{cartoon_id}/', method='PATCH', data=kwargs)
+
+class EpisodesClient(Base):
+    def get_episodes(self, **kwargs):
+        return self.request(endpoint='v1/cartoons/episodes/', params=kwargs)
+    
+    def add(self, **kwargs):
+        return self.request(endpoint='v1/cartoons/episodes/', method='POST', data=kwargs)
+    
+    def detail(self, episode_id: int):
+        return self.request(endpoint=f'v1/cartoons/episodes/{episode_id}/')
+    
+    def update(self, episode_id:int, **kwargs):
+        return self.request(endpoint=f'v1/cartoons/episodes/{episode_id}/', method='PATCH', data=kwargs)
