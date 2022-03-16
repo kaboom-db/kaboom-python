a41c90968d73ec1a2c6ee8039d91e30838ac5b74
Tanveer Najib <tanveernajib@gmail.com>
2022-03-16 13:55:11 +0000

    added follows func

--- kaboompython/accounts.py
+++ kaboompython/accounts.py
@@ -13,6 +13,18 @@ class UsersClient(Base):
     def report(self, **kwargs):
         return self.request(endpoint=f'v1/accounts/report/', method='POST', data=kwargs)
 
+    def follow(self, username: str, **kwargs):
+        return self.request(endpoint=f'v1/accounts/users/{username}/follow/', method='POST', data=kwargs)
+
+    def unfollow(self, username: str, **kwargs):
+        return self.request(endpoint=f'v1/accounts/users/{username}/unfollow/', method='POST', data=kwargs)
+
+    def followers(self, username: str):
+        return self.request(endpoint=f'v1/accounts/users/{username}/followers/')
+
+    def followings(self, username: str):
+        return self.request(endpoint=f'v1/accounts/users/{username}/following/')
+
 class ImageClient(Base):
     def upload(self, file, **kwargs):
         return self.request(endpoint='v1/accounts/upload/', method='POST', data=kwargs, file=file)
