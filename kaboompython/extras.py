f388bd4cf977bac7baa517e7a3a0a5ce4ce258c4
Tanveer Najib <tanveernajib@gmail.com>
2022-03-16 11:34:57 +0000

    added extras

---
+++ kaboompython/extras.py
@@ -0,0 +1,55 @@
+STATUS_OPTIONS = {
+    'completed': 'COMPLETED',
+    'releasing': 'RELEASING',
+    'planned': 'PLANNED',
+    'cancelled': 'CANCELLED'
+}
+
+IMAGE_REQUESTS_FIELD_OPTIONS = {
+    'cover': 'COVER',
+    'background': 'BACKGROUND',
+    'generic': 'GENERIC',
+    'logo': 'LOGO',
+    'screenshot': 'SCREENSHOT'
+}
+
+IMAGE_REQUESTS_OPTIONS = {
+    'staff': 'comics_Staff',
+    'publisher': 'comics_Publisher',
+    'comic': 'comics_Comic',
+    'issue': 'comics_Issue',
+    'voiceactor': 'cartoons_VoiceActor',
+    'network': 'cartoons_Network',
+    'character': 'cartoons_Character',
+    'cartoon': 'cartoons_Cartoon',
+    'episode': 'cartoons_Episode',
+    'team': 'cartoons_Team'
+}
+
+REPORT_OPTIONS = {
+    'publisher': 'comics_Publisher',
+    'staff': 'comics_Staff',
+    'comic': 'comics_Comic',
+    'issue': 'comics_Issue',
+    'voiceactor': 'cartoons_VoiceActor',
+    'network': 'cartoons_Network',
+    'character': 'cartoons_Character',
+    'cartoon': 'cartoons_Cartoon',
+    'episode': 'cartoons_Episode',
+    'team': 'cartoons_Team',
+    'thought': 'users_Thought',
+    'comment': 'users_Comment',
+    'user': 'auth_User'
+}
+
+CHARACTER_STATUS_OPTIONS = {
+    'unknown': 'UNKNOWN',
+    'alive': 'ALIVE',
+    'deceased': 'DECEASED'
+}
+
+CHARACTER_ALIGNMENT_OPTIONS = {
+    'anti-hero': 'ANTI-HERO',
+    'good': 'GOOD',
+    'evil': 'EVIL'
+}
