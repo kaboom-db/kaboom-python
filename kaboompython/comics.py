5a7a2d187e8a36ebc0efd50925bf98f6f13a8234
Tanveer Najib <tanveernajib@gmail.com>
2022-03-16 10:53:53 +0000

    add staff client

--- kaboompython/comics.py
+++ kaboompython/comics.py
@@ -38,3 +38,16 @@ class PublishersClient(Base):
     
     def update(self, publisher_id: int, **kwargs):
         return self.request(endpoint=f'v1/comics/publishers/{publisher_id}/', method='PATCH', data=kwargs)
+
+class StaffClient(Base):
+    def get_staff(self, **kwargs):
+        return self.request(endpoint='v1/comics/staff/', params=kwargs)
+
+    def add(self, **kwargs):
+        return self.request(endpoint='v1/comics/staff/', method='POST', data=kwargs)
+    
+    def detail(self, staff_id: int):
+        return self.request(endpoint=f'v1/comics/staff/{staff_id}/')
+    
+    def update(self, staff_id: int, **kwargs):
+        return self.request(endpoint=f'v1/comics/staff/{staff_id}/', method='PATCH', data=kwargs)
